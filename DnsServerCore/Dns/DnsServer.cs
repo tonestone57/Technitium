932c72cd1727865c04a30c4a30be17d11041fc71
Zafer Balkan <zafer@zaferbalkan.com>
2026-02-20 15:06:01 +0200

    Formatting

--- DnsServerCore/Dns/DnsServer.cs
+++ DnsServerCore/Dns/DnsServer.cs
@@ -1581,7 +1581,6 @@ namespace DnsServerCore.Dns
                         dueTime: TimeSpan.FromMinutes(5),
                         period: TimeSpan.FromHours(_dnsCookiesRotationPeriodHours));
                 }
-
             }
         }
 
@@ -2661,7 +2660,7 @@ namespace DnsServerCore.Dns
                     else
                     {
                         // CC+SC present
-                        // v1 requires totalLen == 24 (CC 8 + SC 16). Anything else is “not a valid server requestCookie”.
+                        // v1 requires totalLen == 24 (CC 8 + SC 16). Anything else is “not a valid server cookie”.
                         bool looksLikeV1 = requestCookie.ServerCookie[0] == 1;
                         if (looksLikeV1 && scLen != 16)
                         {
